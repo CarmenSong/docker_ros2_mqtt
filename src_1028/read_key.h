0f190d7092fc7d1bd8e6e79a3b78252e30cccc23
CarmenSong <102047534+CarmenSong@users.noreply.github.com>
2022-10-28 01:24:51 +0900

    Create read_key.h

---
+++ src_1028/read_key.h
@@ -0,0 +1,67 @@
+#include <signal.h>
+#include <termios.h>
+#include <stdio.h>
+#include <memory.h>
+#include <unistd.h>
+#include <string>
+
+int kfd = 0;
+struct termios cooked, raw;
+extern std::string received_msg;
+
+#define KEYCODE_R 0x43
+#define KEYCODE_L 0x44
+#define KEYCODE_U 0x41
+#define KEYCODE_D 0x42
+#define KEYCODE_Q 0x71
+#define KEYCODE_C 0x45
+
+void quit(int sig)
+{
+        (void)sig;
+        tcsetattr(kfd, TCSANOW, &cooked);
+        exit(0);
+}
+
+void keyboard_init(void)
+{
+        signal(SIGINT, quit);
+
+        // get the console in raw mode
+        tcgetattr(kfd, &cooked);
+        memcpy(&raw, &cooked, sizeof(struct termios));
+        raw.c_lflag &= ~(ICANON | ECHO);
+        // Setting a new line, then end of file
+        raw.c_cc[VEOL] = 1;
+        raw.c_cc[VEOF] = 2;
+        tcsetattr(kfd, TCSANOW, &raw);
+}
+
+std::string read_keyboard()
+{
+        char c;
+        static std::string key;
+        // get the next event from the keyboard
+        if (::read(kfd, &c, 1) < 0)
+        {
+                perror("read():");
+                exit(-1);
+        }
+
+        switch (c)
+        {
+        case KEYCODE_L:
+                key = std::to_string(c); break;
+        case KEYCODE_R:
+                key = std::to_string(c); break;
+        case KEYCODE_U:
+                key = std::to_string(c); break;
+        case KEYCODE_D:
+                key = std::to_string(c); break;
+        case KEYCODE_C:
+                key = std::to_string(c); break;
+        default: break;
+        }
+        // std::string key = std::to_string(c);
+        return key;
+}
